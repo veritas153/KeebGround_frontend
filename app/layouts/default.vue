cd9af7daa57a6896fd146b6e08cf15b5116d1f67
vanytas <vanytas@vanytasui-MacBookPro.local>
2024-09-17 15:16:52 +0900

    [#3] feat: sentence practice page set

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -4,32 +4,25 @@ const appConfig = useAppConfig()
 const { isHelpSlideoverOpen } = useDashboard()
 
 const links = [{
-  id: 'home',
-  label: 'Home',
-  icon: 'i-heroicons-home',
-  to: '/',
+  id: 'sentence_practice',
+  label: 'Sentance practice',
+  to: '/sentence',
   tooltip: {
-    text: 'Home',
-    shortcuts: ['G', 'H']
+    text: 'Sentence practice'
   }
 }, {
-  id: 'inbox',
-  label: 'Inbox',
-  icon: 'i-heroicons-inbox',
+  id: 'paragraph_practice',
+  label: 'Paragraph practice',
   to: '/inbox',
-  badge: '4',
   tooltip: {
-    text: 'Inbox',
-    shortcuts: ['G', 'I']
+    text: 'Paragraph practice'
   }
 }, {
-  id: 'users',
-  label: 'Users',
-  icon: 'i-heroicons-user-group',
-  to: '/users',
+  id: 'record',
+  label: 'Record',
+  to: '/record',
   tooltip: {
-    text: 'Users',
-    shortcuts: ['G', 'U']
+    text: 'record'
   }
 }, {
   id: 'settings',
@@ -80,8 +73,6 @@ const groups = [{
   }]
 }]
 
-const defaultColors = ref(['green', 'teal', 'cyan', 'sky', 'blue', 'indigo', 'violet'].map(color => ({ label: color, chip: color, click: () => appConfig.ui.primary = color })))
-const colors = computed(() => defaultColors.value.map(color => ({ ...color, active: appConfig.ui.primary === color.label })))
 </script>
 
 <template>
@@ -101,18 +92,9 @@ const colors = computed(() => defaultColors.value.map(color => ({ ...color, acti
       </UDashboardNavbar>
 
       <UDashboardSidebar>
-        <template #header>
-          <UDashboardSearchButton />
-        </template>
+
 
         <UDashboardSidebarLinks :links="links" />
-
-        <UDivider />
-
-        <UDashboardSidebarLinks
-          :links="[{ label: 'Colors', draggable: true, children: colors }]"
-          @update:links="colors => defaultColors = colors"
-        />
 
         <div class="flex-1" />
 
---
+++ app/pages/sentence.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { sub } from 'date-fns'
+import type { Period, Range } from '~/types'
+
+const { isNotificationsSlideoverOpen } = useDashboard()
+
+const range = ref<Range>({ start: sub(new Date(), { days: 14 }), end: new Date() })
+const period = ref<Period>('daily')
+</script>
+
+<template>
+  <UDashboardPage>
+    <UDashboardPanel grow>
+      <UDashboardNavbar title="Sentence Practice">
+        
+      </UDashboardNavbar>
+
+    </UDashboardPanel>
+  </UDashboardPage>
+</template>
