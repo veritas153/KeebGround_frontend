6715962773063201e0dba1ebf8f0940a6e30980b
vanytas <vanytas@vanytasui-MacBookPro.local>
2024-09-17 12:00:14 +0900

    [#2] feat: nuxt.js template added. testing.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,47 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  extends: ['@nuxt/ui-pro'],
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/ui',
+    '@vueuse/nuxt'
+  ],
+
+  ui: {
+    safelistColors: ['primary', 'red', 'orange', 'green']
+  },
+
+  colorMode: {
+    disableTransition: true
+  },
+
+  routeRules: {
+    // Temporary workaround for prerender regression. see https://github.com/nuxt/nuxt/issues/27490
+    '/': { prerender: true }
+  },
+
+  devtools: {
+    enabled: true
+  },
+
+  typescript: {
+    strict: false
+  },
+
+  future: {
+    compatibilityVersion: 4
+  },
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  },
+
+  compatibilityDate: '2024-07-11'
 })
